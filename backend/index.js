39f0d1d5919eeab93975743646b864cc73efc06c
Fong Nguyen <fongnguyen122@gmail.com>
2023-08-09 21:56:03 +0700

    connect mongodb

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,21 @@ const routesHandler = require('./routes/handler.js');
 const mongoose = require('mongoose');
 require('dotenv/config');
 
+//routes
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+app.use('/', routesHandler);
 
-app.get('/', (req, res) => {
-    res.send('Hello!')
-})
+//mongodb connect
+mongoose.connect(process.env.DB_URI, {useNewUrlParser:true, useUnifiedTopology:true}).then( () => {
+    console.log(`Mongodb connected!`);
+}).catch( (err) => {
+    console.log(err);
+});
 
+//run server backend
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server port: ${PORT}`)
-})
+    console.log(`Server port: ${PORT}`);
+});
---
+++ backend/models/Schemas.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    // _id: ObjectId
+    username: {type:String, required:true},
+    fullname: {type:String, required:true},
+    entryDate: {type:Date, default:Date.now}
+});
+
+const newsSchema = new Schema({
+    // _id:
+    title: {type:String, required:true},
+    content: {type:String, required:true},
+    user: {type:Schema.Types.ObjectId, ref:'users'},
+    entryDate: {type:Date, default:Date.now}
+})
+
+const Users = mongoose.model('users', userSchema, 'users');
+const News = mongoose.model('news', newsSchema, 'news');
+const mySchemas = {'Users':Users, 'News':News};
+
+module.exports = mySchemas;
