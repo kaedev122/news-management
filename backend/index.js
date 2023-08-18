c50427501286e1f38dac9e78c4c3454af3ebee17
kaedev <92576346+kaedev122@users.noreply.github.com>
2023-08-18 22:27:59 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,20 +6,11 @@ require('dotenv/config');
 
 //routes
 const app = express();
-app.use(cors(
-    {
-        origin: ["https://news-management-chsw.vercel.app"],
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-        credentials: true
-));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use('/', routesHandler);
-app.options('/*', (_, res) => {
-    res.sendStatus(200);
-});
+
 //mongodb connect
 const mongodb_uri = process.env.DB_URI; 
 mongoose.connect(mongodb_uri, {useNewUrlParser:true, useUnifiedTopology:true}).then( () => {
